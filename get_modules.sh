194e59623a8b24c64e7949139bd60812de6938da
Andy Edmonds <andy@edmonds.be>
2013-10-03 01:31:15 +0200

    add newrelic

--- get_modules.sh
+++ get_modules.sh
@@ -4,3 +4,4 @@ mkdir modules
 echo "*" > modules/.gitignore
 puppet module install puppetlabs/openstack --target-dir ./modules
 puppet module install puppetlabs/ntp --target-dir ./modules
+git clone http://github.com/joemiller/puppet-newrelic modules/newrelic
