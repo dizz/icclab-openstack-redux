c394a8bb6323e8dbde1fcc2f62bbf3d910a98f96
Andy Edmonds <andy@edmonds.be>
2013-10-03 00:31:21 +0200

    more minors

--- get_modules.sh
+++ get_modules.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
 mkdir modules
+echo "*" > modules/.gitignore
 puppet module install puppetlabs/openstack --target-dir ./modules
 puppet module install puppetlabs/ntp --target-dir ./modules
