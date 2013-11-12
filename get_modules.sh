d6c06d93b6fcd169402e96077f51f612f0970ac1
Andy Edmonds <andy@edmonds.be>
2013-11-12 21:11:52 +0100

    minor updates for havana

--- get_modules.sh
+++ get_modules.sh
@@ -8,11 +8,12 @@ puppet module install puppetlabs/lvm --target-dir ./modules
 git clone http://github.com/joemiller/puppet-newrelic modules/newrelic
 git clone https://github.com/stackforge/puppet-ceilometer.git modules/ceilometer
 git clone https://github.com/dizz/icclab-puppet-savanna.git modules/savanna
-# git clone https://github.com/stackforge/puppet-heat.git modules/heat
-git clone http://github.com/dizz/icclab-os modules/icclab
+git clone https://github.com/stackforge/puppet-heat.git modules/heat
+git clone http://github.com/dizz/icclab-os -b havana modules/icclab 
 
 # puppet module install puppetlabs/openstack --target-dir ./modules
-git clone git@github.com:dizz/puppet-openstack.git modules/openstack
+# git clone git@github.com:dizz/puppet-openstack.git modules/openstack
+git clone https://github.com/stackforge/puppet-openstack.git modules/openstack
 gem install librarian-puppet
 cd modules/openstack
 librarian-puppet install --path ../
