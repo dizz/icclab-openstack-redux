4e3acb1e2d6e64a28402401aaa847fdbd785aad4
Andy Edmonds <andy@edmonds.be>
2013-11-30 16:23:29 +0100

    removes old branch reference, adds ryu module

--- get_modules.sh
+++ get_modules.sh
@@ -11,7 +11,7 @@ git clone https://github.com/dizz/icclab-puppet-savanna.git modules/savanna
 git clone https://github.com/stackforge/puppet-heat.git modules/heat
 git clone http://github.com/dizz/icclab-os modules/icclab 
 git clone https://github.com/stackforge/puppet-openstack.git modules/openstack
-git clone https://github.com/dizz/icclab-ryu.git
+git clone https://github.com/dizz/icclab-ryu.git modules/ryu
 
 gem install librarian-puppet
 cd modules/openstack
