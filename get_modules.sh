69aae9e89c7f054a25436b8aed69b2296e904803
Andy Edmonds <andy@edmonds.be>
2013-12-18 22:57:39 +0100

    adds offical puppet havana module

--- get_modules.sh
+++ get_modules.sh
@@ -4,17 +4,17 @@ mkdir modules
 echo "*" > modules/.gitignore
 puppet module install puppetlabs/ntp --target-dir ./modules
 puppet module install puppetlabs/lvm --target-dir ./modules
+puppet module install puppetlabs/openstack --target-dir ./modules --version 3.0.0-rc1
 
-git clone http://github.com/joemiller/puppet-newrelic modules/newrelic
-git clone https://github.com/stackforge/puppet-ceilometer.git modules/ceilometer
-git clone https://github.com/dizz/icclab-puppet-savanna.git modules/savanna
-git clone https://github.com/stackforge/puppet-heat.git modules/heat
-git clone http://github.com/dizz/icclab-os modules/icclab 
-git clone https://github.com/stackforge/puppet-openstack.git modules/openstack
-git clone https://github.com/dizz/icclab-ryu.git modules/ryu
+git clone https://github.com/joemiller/puppet-newrelic modules/newrelic
+git clone https://github.com/stackforge/puppet-ceilometer modules/ceilometer
+git clone https://github.com/stackforge/puppet-savanna modules/savanna
+git clone https://github.com/stackforge/puppet-heat modules/heat
+git clone https://github.com/dizz/icclab-os modules/icclab 
+git clone https://github.com/dizz/icclab-ryu modules/ryu
 
-gem install librarian-puppet
-cd modules/openstack
-librarian-puppet install --path ../
-
-cd ../..
+# git clone https://github.com/stackforge/puppet-openstack -b stable/havana modules/openstack
+# gem install librarian-puppet
+# cd modules/openstack
+# librarian-puppet install --path ../
+# cd ../..
